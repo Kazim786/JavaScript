6b7f02d66db638b227775ebe1ef0f9cf95f5c1bd
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-04 10:38:57 -0600

    rearranging

--- first_lesson.js
+++ first_lesson.js
@@ -3,5 +3,5 @@
 //     "Hello World!";
 // </script> */} this came about because we were using js in another html file (the class.html file)
 
-console.log('Hello') /*to make it run type node and then name of file*/
+console.log('Hello class') /*to make it run type node and then name of file*/
 
