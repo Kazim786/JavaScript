d858853f7694083ab49aa5d871bcef437ee091e2
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-04 10:27:42 -0600

    having fun with js

--- first_lesson.js
+++ first_lesson.js
@@ -1,14 +1,7 @@
-<script>
-    document.body.innerHTML = 
-    "Hello World!";
-</script>
+// {/* <script>
+//     document.body.innerHTML = 
+//     "Hello World!";
+// </script> */} this came about because we were using js in another html file (the class.html file)
 
-console.log('Hello World');
+console.log('Hello') /*to make it run type node and then name of file*/
 
-"Hello!"
-'Hello!'
-`Hello
-There!
-`
-
-"Hello" + " " + "There"
