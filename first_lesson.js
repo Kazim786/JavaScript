6e64c825412d650c52b580c0dad9e3c82a6d8eac
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-04 11:29:45 -0600

    Havin fun

--- first_lesson.js
+++ first_lesson.js
@@ -1,22 +1,48 @@
-// {/* <script>
-//     document.body.innerHTML = 
-//     "Hello World!";
-// </script> */} this came about because we were using js in another html file (the class.html file)
+// // {/* <script>
+// //     document.body.innerHTML = 
+// //     "Hello World!";
+// // </script> */} this came about because we were using js in another html file (the class.html file)
 
-// console.log('Hello class') /*to make it run type node and then name of file*/
+// // console.log('Hello class') /*to make it run type node and then name of file*/
 
-// var school = "digital crafts" 
+// // var school = "digital crafts" 
 
-// doing strings in js^^
+// // doing strings in js^^
 
-// var myString = "Kazim";
-// var myInt = 72;
-// var myBool = true;
-// console.log(myString.length)
-var num = 1; //integer
-var num2 = 1.2; //float
-var myString = "school";
-// console.log(myString.length)
-var concat = "hello" + "world";
-console.log(concat)
-console.log(concat.indexOf("world"));
+// // var myString = "Kazim";
+// // var myInt = 72;
+// // var myBool = true;
+// // console.log(myString.length)
+// var num = 1; //integer
+// var num2 = 1.2; //float
+// var myString = "school";
+// // console.log(myString.length)
+// var concat = "hello" + "world";
+// console.log(concat)
+// console.log(concat.indexOf("world"));
+
+// var boolean = !true; //to make false add ! in front of true
+// console.log(boolean);
+// var not = !boolean //now boolean will return back to being true. remember ! makes boolean do opposite
+
+// var and = boolean && false; //you need two ampersands for ANDS 
+// console.log(and)
+
+// var or = boolean || false; //you need 2 pipes for OR 
+// console.log(or)
+
+//to write a condition:
+
+// var a = 4
+// var b = 4
+
+// if(a == b){
+//     console.log("Equality")
+
+// }
+
+var age = 21
+
+if (age >= 21){
+    console.log("You are allowed to make a membership")
+}
