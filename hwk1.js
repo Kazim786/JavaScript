787477e33c0ddae2741dc39681d93f4cb2ca5251
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-05 00:17:10 -0600

    making progress

--- hwk1.js
+++ hwk1.js
@@ -1,4 +1,6 @@
 function madlib(name, subject){
-var output = "Hello my name" + name + "is, I am taking " + subject 
+var output = "Hello my name" + name + "is, I am taking " + subject;
+console.log(output);
+return output; 
 
 } 
