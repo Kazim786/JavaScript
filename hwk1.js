94a0c4f0ada8f1e458cc1170f53cce5dc110665f
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-05 00:22:39 -0600

    it worked

--- hwk1.js
+++ hwk1.js
@@ -1,6 +1,10 @@
 function madlib(name, subject){
 var output = "Hello my name" + name + "is, I am taking " + subject;
-console.log(output);
+// console.log(output);
 return output; 
 
 } 
+var Kazim = "Kazim";
+var DigitalCrafts = "DigitalCrafts";
+var myExample = "I am " + Kazim + "in " + DigitalCrafts;
+console.log(madlib(Kazim, DigitalCrafts));
