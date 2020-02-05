a7a018808e0cd3816de874eca0bcce80f723f95a
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-05 00:33:36 -0600

    question 2

--- hwk1.js
+++ hwk1.js
@@ -1,10 +1,17 @@
-function madlib(name, subject){
-var output = "Hello my name" + name + "is, I am taking " + subject;
-// console.log(output);
-return output; 
+//1
+// function madlib(name, subject){
+// var output = "Hello my name" + name + "is, I am taking " + subject;
+// // console.log(output);
+// return output; 
 
-} 
-var Kazim = "Kazim";
-var DigitalCrafts = "DigitalCrafts";
-var myExample = "I am " + Kazim + "in " + DigitalCrafts;
-console.log(madlib(Kazim, DigitalCrafts));
+// } 
+// var Kazim = "Kazim";
+// var DigitalCrafts = "DigitalCrafts";
+// console.log(madlib(Kazim, DigitalCrafts));
+
+//2
+function tipAmount(){
+    prompt("What was the level of your service? Good, Fair, or bad?");
+
+
+}
