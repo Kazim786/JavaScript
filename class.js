ba24b0a6a3a7a4c0483120358e321c05a7e46ac8
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-06 14:04:14 -0600

    filtering array

--- class.js
+++ class.js
@@ -90,7 +90,7 @@
 //Map
 
 //The old way
-let arr = [0, 1, 2, 3, 4, 5];
+// let arr = [0, 1, 2, 3, 4, 5];
 // let newArr = []
 // for (let index = 0; index < arr.length; index++){
 //     newArr.push(2 * arr[index]);
@@ -104,21 +104,30 @@ let arr = [0, 1, 2, 3, 4, 5];
 
 // console.log(newArr);
 
-const companies = [
-    {name: "Company One", category: "Finance", start: 1981, end: 2003},
-    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-    ];
+// const companies = [
+//     {name: "Company One", category: "Finance", start: 1981, end: 2003},
+//     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+//     {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+//     {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+//     {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+//     {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+//     {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+//     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+//     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+//     ];
 
-    let newArray = companies.map(function(element){
-        element.start = element.start - 5
-        return element
+//     let newArray = companies.map(function(element){
+//         element.start = element.start - 5
+//         return element
 
-    })
-    console.log(newArray)
+//     })
+//     console.log(newArray)
+
+//Array filter
+
+let arr = [0, 10, 2, 30, 4, 5];
+let newArr = arr.filter(function(element){
+
+    return element < 10;
+})
+console.log(newArr); //will put into newArr [0, 2, 4, 5]
