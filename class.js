336b92bde4d42adad4b286a3391d2be7c3a074f5
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-06 14:18:30 -0600

    some

--- class.js
+++ class.js
@@ -132,21 +132,26 @@
 // })
 // console.log(newArr); //will put into newArr [0, 2, 4, 5]
 
-const companies = [
-        {name: "Company One", category: "Finance", start: 1981, end: 2003},
-        {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-        {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-        {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-        {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-        {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-        {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-        {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-        {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-        ];
+// const companies = [
+//         {name: "Company One", category: "Finance", start: 1981, end: 2003},
+//         {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+//         {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+//         {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+//         {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+//         {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+//         {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+//         {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+//         {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+//         ];
     
-        let newArray = companies.filter(function(element){
-            return element.category == "Retail" 
-        })
-        console.log(newArray);
+//         let newArray = companies.filter(function(element){
+//             return element.category == "Retail" 
+//         })
+//         console.log(newArray);
+//Some array, returns true or false.
+let arr = [0, 1, 2, 3, 4, 5];
+let someLT2 = arr.some(function(element){
+    return element < 5;
 
-        
+})        
+console.log(someLT2);
