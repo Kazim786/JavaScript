c33fe3e1896ebba667f8c7ad400d4c3678b07655
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-06 11:12:45 -0600

    pass by ref/pass by value

--- class.js
+++ class.js
@@ -9,13 +9,25 @@
 
 // var result = add(4, 5);
 
-var a = 4;
-var b = 5;
+// var a = 4;
+// var b = 5;
 
-var c = a;
+// var c = a;
 
-c = 88;
+// c = 88;
 
-console.log(a);
-console.log(c);
+// console.log(a);
+// console.log(c);
+
+myArrayOrig = [1, 2, 3, 6, 7];
+
+myArrayCopy = myArrayOrig;
+
+console.log(myArrayOrig);
+console.log(myArrayCopy);
+myArrayOrig[0] = 99;
+console.log(myArrayOrig);
+console.log(myArrayCopy);
+//the myArrayCopy also changed because its being passed by reference.
+
 
