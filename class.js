5b71fa1a1a40b0db0b4720b63fb4f4cd45bd704c
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-06 13:57:06 -0600

    map function

--- class.js
+++ class.js
@@ -60,15 +60,49 @@
 
 //Arrays
 
-let arr = [2, 5, 6, 7, 8];
+// let arr = [2, 5, 6, 7, 8];
 
-for (let index = 0; index < arr.length; index++){
-    console.log(arr[index])
-}
+// for (let index = 0; index < arr.length; index++){
+//     console.log(arr[index])
+// }
 
-arr.forEach(function(element){
-    console.log(element);
-})
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+
+// const companies = [
+//     {name: "Company One", category: "Finance", start: 1981, end: 2003},
+//     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+//     {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+//     {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+//     {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+//     {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+//     {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+//     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+//     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+//     ];
+//     const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//     companies.forEach(function(company){
+//         console.log(company.name)
+//     })
+
+//Map
+
+//The old way
+let arr = [0, 1, 2, 3, 4, 5];
+// let newArr = []
+// for (let index = 0; index < arr.length; index++){
+//     newArr.push(2 * arr[index]);
+
+// }
+
+// let newArr = arr.map(function(element){
+//     return 2 * element;
+
+// })
+
+// console.log(newArr);
 
 const companies = [
     {name: "Company One", category: "Finance", start: 1981, end: 2003},
@@ -81,8 +115,10 @@ const companies = [
     {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
     ];
-    const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-    companies.forEach(function(company){
-        console.log(company.name)
+    let newArray = companies.map(function(element){
+        element.start = element.start - 5
+        return element
+
     })
+    console.log(newArray)
