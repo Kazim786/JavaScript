1007b5d90bc5e68b0c03d3adc4455517abbc477d
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-06 14:14:13 -0600

    filtering

--- class.js
+++ class.js
@@ -125,9 +125,28 @@
 
 //Array filter
 
-let arr = [0, 10, 2, 30, 4, 5];
-let newArr = arr.filter(function(element){
+// let arr = [0, 10, 2, 30, 4, 5];
+// let newArr = arr.filter(function(element){
 
-    return element < 10;
-})
-console.log(newArr); //will put into newArr [0, 2, 4, 5]
+//     return element < 10;
+// })
+// console.log(newArr); //will put into newArr [0, 2, 4, 5]
+
+const companies = [
+        {name: "Company One", category: "Finance", start: 1981, end: 2003},
+        {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+        {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+        {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+        {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+        {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+        {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+        {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+        {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+        ];
+    
+        let newArray = companies.filter(function(element){
+            return element.category == "Retail" 
+        })
+        console.log(newArray);
+
+        
